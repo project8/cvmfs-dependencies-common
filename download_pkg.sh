8f1042c77fec734169db7200ef809698cf01463b
Guigue, Mathieu <mathieu.guigue@pnnl.gov>
2017-11-12 23:10:27 -0800

    Cleaning up the mess and adding a proper documentation

--- download_pkg.sh
+++ download_pkg.sh
@@ -30,11 +30,6 @@ gunzip cmake-3.4.3.tar.gz             # cmake
 tar -xf cmake-3.4.3.tar
 ln -s cmake-3.4.3 cmake
 
-# mv download boost_1_59_0.tar.gz       # boost
-# gunzip boost_1_59_0.tar.gz
-# tar -xf boost_1_59_0.tar
-# ln -s boost_1_59_0 boost
-
 gunzip hdf5-1.8.18.tar.gz             # hdf5
 tar -xf hdf5-1.8.18.tar
 ln -s hdf5-1.8.18 hdf5
@@ -46,10 +41,6 @@ ln -s fftw-3.3.4 fftw
 gunzip matio-1.5.2.tar.gz             # matio
 tar -xf matio-1.5.2.tar
 ln -s matio-1.5.2 matio
-
-# gunzip libpng-1.6.34.tar.gz             # libpng
-# tar -xf libpng-1.6.34.tar
-# ln -s libpng-1.6.34 libpng
 
 gunzip root_v6.10.06.source.tar.gz    # root
 tar -xf root_v6.10.06.source.tar
